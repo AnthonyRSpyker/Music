cf827d1ac2c5271de3edc08bb5e26b6e82c76eb6
Anthony R. Spyker <anthonyspyker@gmail.com>
2021-01-16 12:54:23 -0500

    made a genaric ajax requst for purposes of scaffolding moving forward

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,22 @@ const song = $("#song_search");
 //id's for song and artist buttons
 const songButton = $("#song-search-button");
 const artistButton = $("#artist-search-button");
+
+//genaric search needs to be made dynamic
+var art = "yes";
+var track = "roundabout";
+var urlQuery = "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=" + track + "&q_artist=" + art + "&apikey=7f6c68b406143881580235194e8517a0"
+
+
+$.ajax({
+    url: urlQuery,
+    dataType: "jsonp",
+    method: "GET"
+})
+.then(function(artist){
+    console.log(artist)
+    //specific lyrics request, have to make the lyrics populate somewhere on page.
+    var lyrics = artist.message.body.lyrics.lyrics_body
+    console.log(lyrics)
+    var 
+})
