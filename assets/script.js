f2b65c272612a7c2d48536252e4791cc62e73385
jlw429 <jlw00329@gmail.com>
2021-01-16 13:59:36 -0500

    added jq hide functunality and commented out show for click events

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
+//added document ready call
+$(document).ready(function () {
+  //hide the elements until they are populated.
+  $('.artist-cards').hide();
+  $('.lyrics-video-cards').hide();
+
+  document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.autocomplete');
     var instances = M.Autocomplete.init(elems, options);
   });
+    //For use in the respective click events (This shows the cards that I hid at the beginning)
+    //   $('.artist-cards').hide();
+    //   $('.lyrics-video-cards').hide();
+});
