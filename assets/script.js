010e9e926b04ab9bf3745a98f5634ef089920d4c
Anthony R. Spyker <anthonyspyker@gmail.com>
2021-01-16 13:57:02 -0500

    lyrics will populate a given area on the html

--- assets/script.js
+++ assets/script.js
@@ -8,20 +8,22 @@ const songButton = $("#song-search-button");
 const artistButton = $("#artist-search-button");
 
 //genaric search needs to be made dynamic
-var art = "yes";
-var track = "roundabout";
+var art = "steely dan";
+var track = "dirty work";
 var urlQuery = "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=" + track + "&q_artist=" + art + "&apikey=7f6c68b406143881580235194e8517a0"
 
-
+function lyrics(){
 $.ajax({
     url: urlQuery,
     dataType: "jsonp",
     method: "GET"
 })
 .then(function(artist){
-    console.log(artist)
+    console.log(artist);
     //specific lyrics request, have to make the lyrics populate somewhere on page.
-    var lyrics = artist.message.body.lyrics.lyrics_body
-    console.log(lyrics)
-    var 
-})
+    var lyrics = artist.message.body.lyrics.lyrics_body;
+    console.log(lyrics);
+    $("#lyrics-text").append(lyrics);
+});
+}
+lyrics();
